e51f80c5ec99f763ed2902421233f4752fe65c26
Skyexy <oniprogress1234@gmail.com>
2022-03-11 14:13:03 -0800

    fitx

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - this is for discription
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char a = 'a';
+	char b = 'A';
+	int num = 1;
+	while (num <= 54)
+	{
+		if (num <= 26)
+		{
+			putchar(a++);
+		}
+		else if (num > 26)
+		{
+			putchar(b++);
+		}
+		num++;
+	}
+	putchar('\n')
+	return (0);
+}
