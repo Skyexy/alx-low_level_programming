10eaf5e00f2630041375ffa97fc4a9d2d1b80f86
Skyexy <oniprogress1234@gmail.com>
2022-03-11 14:55:44 -0800

    lo

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - iprope
+ *
+ * Return: 0 when suscess
+ */
+int main(void)
+{
+	int num = 0;
+
+	for (; num <= 10;)
+	{
+		printf("%d\n", num);
+		num++;
+	}
+	return (0);
+}
