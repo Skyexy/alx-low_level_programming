53b04a87c1071f45b61083136613e4617d8da7bc
Skyexy <oniprogress1234@gmail.com>
2022-03-11 16:15:25 -0800

    mop

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - iponme
+ *
+ * Process: 0 sucess
+ *
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	for (; a < 10;)
+	{
+		putchar(a + '0');
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
