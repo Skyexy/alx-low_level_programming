5f57859aa57bed8751e6194388b4aff06e7bec84
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-03-12 04:29:28 +0100

    ddd

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - array
+*
+*Return: 0 for sucess
+*/
+int main()
+{
+	int a = 0;
+	
+	putchar(a + '0')
+	while (a < 10)
+	{
+		a++;
+		putchar(',')
+		putchar(a + '0');
+		putchar(' ');
+		
+	}
+	return 0;
+}
