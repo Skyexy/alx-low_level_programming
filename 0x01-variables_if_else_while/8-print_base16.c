01b1b526efc943b99426c9bfb233070fb698fa91
Skyexy <oniprogress1234@gmail.com>
2022-03-11 19:06:50 -0800

    su

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - buid hexadecimal
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
 {
-  int a = 1
-  char b = 'a'
-   
-   for (a <= 16)
-   {
-    if (a <= 10)
-    {
-      putchar(a + '0')
-    }
-     if (a > 10)
-     {
-      putchar(b)
-      b++
-     }
-    a++
-   }
-   return 0;
+	int a = 1;
+	char b = 'a';
+
+	for (a <= 16)
+	{
+		if (a <= 10)
+		{
+			putchar(a + '0');
+		}
+		if (a > 10)
+		{
+			putchar(b);
+			b++;
+		}
+		a++;
+	}
+	return (0);
 }
