eaab64de7a5a8f4a46b2ca919ea7b5fe2f320d9d
Skyexy <oniprogress1234@gmail.com>
2022-03-17 14:44:45 -0700

    oi

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
- *
  * main - causes an infinite loop
  *
  * Return: 0
@@ -11,8 +10,8 @@
 int main(void)
 {
 	int i;
+
 	printf("Infinite loop incoming :(\n");
-	
 	i = 0;
 	/*
 	 *
