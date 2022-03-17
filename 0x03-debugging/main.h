65c62943ffdff961b83d471a81423fca7ee91ddb
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-03-17 23:19:24 +0100

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,6 +6,7 @@
 void positive_or_negative(int i);
 int main(void);
 int largest_number(int a, int b, int c);
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
