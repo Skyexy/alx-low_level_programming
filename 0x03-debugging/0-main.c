60559b3a6a8ff9de7e3967ba1b7ebf7e13bbb6d6
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-03-17 20:50:18 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,24 +1,16 @@
-#include <time.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * main -Positive anything is better than negative nothing
- *
- * Return: Always 0 (success)
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
-	int n;
+        int i;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n > 0)
-		printf("%d is positive\n", n);
-	if (n == 0)
-		printf("%d is zero\n", n);
-	if (n < 0)
-		printf("%d is negative\n", n);
-	return (0);
+        i = 98;
+        positive_or_negative(i);
+
+        return (0);
 }
