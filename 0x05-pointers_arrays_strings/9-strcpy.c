29ff48ca450b4321f85bf4eaf213a5531a72f6c1
Skyexy <oniprogress1234@gmail.com>
2022-03-21 21:13:37 -0700

    qac

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,12 +1,14 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_array - function that prints n elements of an array
- * of integers, followed by a new line.
- * @a: int pointer variable
- * @n: int type variable
+ * _strcpy - a function that copies the string pointed to by src,
+ * including the terminating null byte (\0), to the buffer
+ * @src: char pointer variable
+ * @dest: char pointer variable
+ * Return: a sequence of characters
  */
+
+
 
 char *_strcpy(char *dest, char *src)
 {
