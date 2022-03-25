800c4227297b6add96b69debf4fcfcb962d8916c
Skyexy <oniprogress1234@gmail.com>
2022-03-24 18:40:50 -0700

    asw

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,49 +2,37 @@
 
 /**
  * cap_string - capitalizes all words of a string
- * @sy: this is the string to be capitalized
+ * @str: this is the string to be capitalized
  * Return: sy
  */
 
-char *cap_string(char *sy)
+char *cap_string(char *str)
 {
-	int i;
-	int j = 97;
-	int k = 0;
-	int y;
-	int z = 0;
+	int i, c;
+	int trigger;
+	char nots[] = ",;.!?(){}\n\t\" ";
 
-	for (i = 0; sy[i] != '\0'; i++)
+	for (i = 0, trigger = 0; str[i] != '\0'; i++)
 	{
-		if (sy[i] == 44 || sy[i] == 59 || sy[i] == 46 || sy[i] == 36
-		|| sy[i] == 63 || sy[i] ==
-		34 || sy[i] == 40 || sy[i] == 41 || sy[i] == 123
-		|| sy[i] == 125 || sy[i] == 32 || sy[i] == 9 || sy[i] == 10)
+		if (str[0] > 96 && str[0] < 123)
+			trigger = 1;
+		for (c = 0; nots[c] != '\0'; c++)
 		{
-			i++;
-			if (sy[i] == 44 || sy[i] == 59 || sy[i] == 46 || sy[i] == 36 || sy[i] == 63
-			|| sy[i] == 34 || sy[i] == 40 || sy[i] == 41 || sy[i] == 123
-			|| sy[i] == 125 || sy[i] == 32 || sy[i] == 9 || sy[i] == 10)
+			if (nots[c] == str[i])
+				trigger = 1;
+		}
+		if (trigger)
+		{
+			if (str[i] > 96 && str[i] < 123)
 			{
-				i++;
+				str[i] -= 32;
+				trigger = 0;
 			}
-				if (sy[i] >= 97 && sy[i] <= 122)
-				{
-					while (j != sy[i])
-					{
-						k++;
-						j++;
-					}
-					j = 97;
-					for (y = 'A'; z < k; y++)
-					{
-						z++;
-					}
-					k = 0;
-					z = 0;
-					sy[i] = y;
-				}
+			else if (str[i] > 64 && str[i] < 91)
+				trigger = 0;
+			else if (str[i] > 47 && str[i] < 58)
+				trigger = 0;
 		}
 	}
-	return (sy);
+	return (str);
 }
