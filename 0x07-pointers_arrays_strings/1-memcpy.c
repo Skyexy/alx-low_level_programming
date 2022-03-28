ab4db0084dae1381dbf9539ec6a9914d549fabdd
Skyexy <oniprogress1234@gmail.com>
2022-03-28 14:05:57 -0700

    az

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * _memcpy - a function that copies memory area.
+ * _memcpy - a function that copies memory area
  * @dest: the string i copy to
  * @src: the sring to copy
  * @n: the number of bytes to be copied
+ * Return: dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
