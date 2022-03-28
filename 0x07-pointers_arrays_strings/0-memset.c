0ab7d2f640531bcdd54859f2bc3b7674d0387597
Skyexy <oniprogress1234@gmail.com>
2022-03-28 13:44:22 -0700

    sde

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#Include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * Return: s;
+ * @n:number of bytes of the memory area pointed to by s to be filled
+ * @s:the buffer to be filled
+ * @b: the bytes to fill s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int k;
+
+	for (k = 0; k < n; k++)
+	{
+		s[k] = b;
+	}
+
+	return (s);
+}
