bd60d99aa0ca1f4f881c01be8e2d69ddece0d7d7
Skyexy <oniprogress1234@gmail.com>
2022-03-28 16:50:09 -0700

    av

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strspn - a function that gets the length of a prefix substring.
+ * @s: s to be surfed
+ * @accept: the of this
+ * Return: g
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int y;
+	int k;
+	int g = 0;
+
+	for (y = 0; s[y] != ','; y++)
+	{
+		for (k = 0; accept[k] != '\0'; k++)
+		{
+			if (accept[k] == s[y])
+			{
+				g++;
+				break;
+			}
+		}
+	}
+	return (g);
+}
