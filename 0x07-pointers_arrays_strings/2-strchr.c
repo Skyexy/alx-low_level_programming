f07228f4e07b6edc267b342b7363355b9ee043fc
Skyexy <oniprogress1234@gmail.com>
2022-03-28 16:00:00 -0700

    rt

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,10 +4,11 @@
  * _strchr - a function that locates a character in a string.
  * @s: the string with character
  * @c: the char to be located
- * Return: nothing
+ * Retur: nothing
  */
 char *_strchr(char *s, char c)
 {
+
 	while (s++)
 	{
 		if (*s == c)
