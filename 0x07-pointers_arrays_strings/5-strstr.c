63f1152804712087cc894114083358661b5b1f83
Skyexy <oniprogress1234@gmail.com>
2022-03-28 18:10:24 -0700

    bn

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strstr - locate a substring
+ * @haystack: a string
+ * @needle: string substring
+ * Return: Pointer to beginning of substring, or NULL if not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int y;
+	int p;
+
+	for (y = 0; needle[y] != '\0'; y++)
+	{
+		for (p = 0; haystack[p] != '\0'; p++)
+		{
+			if (needle[y] == haystack[p])
+			{
+				return (haystack + p);
+			}
+		}
+	}
+	return (NULL);
+}
