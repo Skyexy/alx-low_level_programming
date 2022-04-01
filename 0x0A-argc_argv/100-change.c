83982eabac9532dfd6f38be86cb3c7bba054b04a
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-04-01 17:54:40 +0100

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,17 +9,17 @@
  */
 int main(int argc, char *argv[])
 {
-	int n = atoi(argv[1]);
 	int s[5] = {25, 10, 5, 2, 1};
 	int num = 0;
 	int f = 0;
+	int n = atoi(argv[1]);
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	while (n != 0)
+	while (n > 0)
 	{
 		if (n > 0)
 		{
@@ -30,6 +30,10 @@ int main(int argc, char *argv[])
 				continue;
 			}
 			f++;
+		}
+		else
+		{
+			break;
 		}
 	}
 	printf("%d\n", n);
