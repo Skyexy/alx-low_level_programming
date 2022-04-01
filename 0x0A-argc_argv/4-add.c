410acb9e239f1f7118f884af2ee73ea0311ea026
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-04-01 15:07:10 +0100

    Create 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - this print prints the name of a program
+ * @argc: number of arguments
+ * @argv: array of arguments passed
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int n = 0;
+	int o = 1;
+	if (argc <= 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (argc > 1)
+	{
+		if (argv[o] >= '1' || <= '9')
+		{
+			while (o < argc)
+			{
+				n += atoi(argv[0]);
+			}
+		}
+		else 
+		{
+			printf("Error");
+			return (1);
+		}
+	}
+	printf("%d", n);
+	return (0);
+}
