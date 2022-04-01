c85a15f42d9a33b8b125deb803734401cb333cd9
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-04-01 14:20:57 +0100

    Create 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - this print prints the name of a program
+ * @argc: number of arguments
+ * @argv: array of arguments passed
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
