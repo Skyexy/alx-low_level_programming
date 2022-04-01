46f03f992af5db87e392e36bdb679c7c8625ee96
Skyexy <oniprogress1234@gmail.com>
2022-04-01 06:01:45 -0700

    df

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - this print prints the name of a program
  * @argc: number of arguments
@@ -8,5 +9,5 @@
 int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
-	return(0);
+	return (0);
 }
