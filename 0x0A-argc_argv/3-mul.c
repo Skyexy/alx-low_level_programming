4dbbf445467abdea7003d57933108260a319951f
Skyexy <oniprogress1234@gmail.com>
2022-04-01 06:44:38 -0700

    dfg

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - this print prints the name of a program
@@ -6,21 +7,21 @@
  * @argv: array of arguments passed
  * Return: 0
  */
-int main(int argc , char *argv[])
+int main(int argc, char *argv[])
 {
-  int c = 1;
-  int y = 1;
-  
-if (argc <= 1)
-{
-	printf("Error\n");
-	return (1);
-}
-while (c < argc)
-{
-	y *= argv[c];
-	c++;
-}
-printf("%d/n", y);
-return (0);
+	int c = 1;
+	int y = 1;
+
+	if (argc <= 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	while (c < argc)
+	{
+		y *= atoi(argv[c]);
+		c++;
+	}
+	printf("%d\n", y);
+	return (0);
 }
