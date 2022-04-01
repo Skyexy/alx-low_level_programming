ba01982ed2a82df22fd586d7092cdfe8f44baa92
Skyexy <oniprogress1234@gmail.com>
2022-04-01 05:09:06 -0700

    sup

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,16 @@
-include "main.h"
-  
-  /**
-  * this print prints the name of a program
-  * Return: 0
-  */
-int main (argc, char* argv[])
+#include "main.h"
+
+/**
+ * main - this print prints the name of a program
+ * @argc: number of arguments
+ * @argv: array of arguments passed
+ * Return: 0
+ */
+int main(int argc, char *argv[])
 {
-	_putchar(argv[0]);
-	return(0);
+	if (argc >= 1)
+	{
+		_putchar(*(argv[0]));
+	}
+	return (0);
 }
