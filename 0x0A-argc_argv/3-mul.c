2d7290883577924d4be848d0ed459aa2c5138621
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-04-01 20:34:29 +0100

    Create 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - this print prints the name of a program
+ * @argc: number of arguments
+ * @argv: array of arguments passed
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int m;
+	int s;
+	int f;
+	
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	m = atoi(argv[1]);
+	s = atoi(argv[2]);
+	f = m * s;
+	printf("%d\n", f);
+	return (0);
+}
