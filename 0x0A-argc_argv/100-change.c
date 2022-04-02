50aeb071e1ce706be99bf3ea2fe2f0bfd951918f
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-04-02 01:27:19 +0100

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,37 +1,52 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - this print prints the name of a program
- * @argc: number of arguments
- * @argv: array of arguments passed
- * Return: 0
+ * main - print the min number of coins to make change for an amount of money
+ * @argc: argument count
+ * @argv: argument vector, array of strings
+ * Return: 1 if error, 0 otherwise
  */
+
 int main(int argc, char *argv[])
 {
-	int s[5] = {25, 10, 5, 2, 1};
-	int num = 0;
-	int f = 0;
-	int n = atoi(argv[1]);
-	int g;
+	int total, count;
+	unsigned int i;
+	char *p;
+	int cents[] = {25, 10, 5, 2};
 
-	if (argc == 2)
+	if (argc != 2)
 	{
-		while (n > 0)
+		printf("Error\n");
+		return (1);
+	}
+
+	total = strtol(argv[1], &p, 10);
+	count = 0;
+
+	if (!*p)
+	{
+		while (total > 1)
 		{
-			for (g = 0; g < 4; g++, f++)
+			for (i = 0; i < sizeof(cents[i]); i++)
 			{
-				if ((n - s[f]) > 0)
+				if (total >= cents[i])
 				{
-					n -= s[f];
-					num += 1;
-					continue;
+					count += total / cents[i];
+					total = total % cents[i];
 				}
 			}
 		}
-		printf("%d\n", num);
-		return (0);
+		if (total == 1)
+			count++;
 	}
-	printf("Error\n");
-	return (1);
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", count);
+	return (0);
 }
