414ce66904b021cbe9ee774e5395bb01d2eae29d
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-04-04 21:42:22 +0100

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,11 +4,11 @@
 
 char *create_array(unsigned int size, char c)
 {
-    int i = 0;
+    unsigned int i = 0;
     char *m;
     m = (char*)malloc(sizeof(char) * size);
     
-    if (m = NULL)
+    if (*m = NULL)
     {
         return(0);
     }
