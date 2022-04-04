9edb6825b991d0e608f284944717fb7b645a14eb
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-04-04 22:16:36 +0100

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,6 +5,7 @@
 * create_array - this creates an array of charc, with memory of size
 * @size: size of array
 * @c: the char to be put in array
+* Return: m and NULL if size is 0 or prog fails
 */
 char *create_array(unsigned int size, char c)
 {
