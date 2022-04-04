a9950a109d13eed0d7dc1d2128e1ad58dbe8aab7
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-04-04 21:59:46 +0100

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,20 +1,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/*
+* creat_array - this creates an array of chars, and initializes it with a specific char
+* @size: size of array
+* @c: the char to be put in array
+*/
 char *create_array(unsigned int size, char c)
 {
-    unsigned int i = 0;
-    char *m;
-    m = malloc(sizeof(char) * size);
-    
-    if (m == NULL)
-    {
-        return(0);
-    }
-    while (i < size)
-    {
-        m[i] = c;
-        i++;
-    }
-    return(m);
+	unsigned int i = 0;
+	char *m;
+	m = malloc(sizeof(char) * size);/* this is used to allocate dynamic memory to pointer m*/
+
+	if (m == NULL)
+	{
+		return(0);
+	}
+	while (i < size)
+	{
+		m[i] = c;
+		i++;
+	}
+	return(m);
 }
