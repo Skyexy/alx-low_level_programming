eb01bc304acadc309db0ec3c522f390f93d6df4c
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-04-05 02:45:07 +0100

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int y(char* s1, char* s2)
+/**
+ * str_concat - a function that concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: t and 0 if it fails
+ */
+char *str_concat(char *s1, char *s2)
 {
-	int y = 0;
-	int p = 0;
-	int l;
+	int y = 0; int p = 0; int l; char *t;
+	int k = 0;
+	int m = 0;
+
+	if (!s1)
+		s1 = malloc(sizeof(char));
+	if (!s2)
+		s2 = malloc(sizeof(char));
 	while (s1[y] || s2[p] != '\0')
 	{
 		if (s1[y] != '\0')
@@ -18,39 +29,13 @@ int y(char* s1, char* s2)
 		}
 	}
 	l = (y + p);
-	return(ypl);
-}
-/**
- * str_concat - a function that concatenates two strings
- * @s1: first string
- * @s2: second string
- * Return: t and 0 if it fails
- */
-char *str_concat(char *s1, char *s2)
-{
-	char *t;
-	int k = 0;
-	int m = 0;
-	if (!s1)
-	{
-		s1 = malloc(sizeof(char));
-	}
-	if (!s2)
-	{
-		s2 = malloc(sizeof(char));
-	}
-	y(s1, s2);
 	t = malloc((sizeof(char) * l) + 1);
 	if (t == NULL)
-	{
 		return (NULL);
-	}
 	while (k < l)
 	{
 		if (k < y)
-		{
 			t[k] = s1[k];
-		}
 		else if (k >= y)
 		{
 			t[k] = s2[m];
