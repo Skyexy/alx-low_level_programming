a525564ff4d099faa830879ca8139173b060fc2a
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-04-08 00:18:53 +0100

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,6 +10,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
+	unsigned int i = 0;
 
 	if (nmemb == 0 || size == 0)
 	{
@@ -19,6 +20,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (!ptr)
 	{
 		return (NULL);
+	}
+	while (i < size)
+	{
+		p[i] = 0;
+		i++;
 	}
 	return (ptr);
 }
