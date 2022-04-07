f040cf6316bfdb68b6a6dc8849901de23383d314
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-04-08 00:33:27 +0100

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 
 /**
-<<<<<<< HEAD
  * sup - this changes array
  * @s: this is a pointer
  * @b: this is a character
@@ -10,14 +9,6 @@
  * Return: s
  */
 char sup(char *s, char b, unsigned int i)
-=======
-sup - this changes array
-@s: this is a pointer
-@b: this is a character
-@i: this is an integer
-Return: s
-char *sup(char *s, char b, unsigned int i)
->>>>>>> 73b7e5efdd79f9ac028e8052a4ecf92da09ef767
 {
 	unsigned int p = 0;
 	
