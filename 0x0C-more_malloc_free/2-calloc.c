4a7255a7a7ff1174815d9ad8c0043f4b862de71c
Skyexy <oniprogress1234@gmail.com>
2022-04-07 16:13:47 -0700

    lpoe

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,14 +2,15 @@
 #include <stdlib.h>
 
 /**
-_calloc - allocates memory for an array, using malloc.
-@nmemb: size of memry
-@size: number of mem in byts
-Return: ptr
+ * _calloc - allocates memory for an array, using malloc.
+ * @nmemb: size of memry
+ * @size: number of mem in byts
+ * Return: ptr
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
-	
+
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
