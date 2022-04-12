2abad4b32084bbfba9d4303e73f0daf24df74c9e
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-04-12 18:20:39 +0100

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,1 +1,33 @@
+#include "dog.h"
+#include <stdlib.h>
 
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+		{
+			prinf("Name: (nil)\n");
+		}
+		else if (d->name != NULL)
+		{
+			prinf("Name: %s\n", d->name);
+		}
+		if (d->age == NULL)
+		{
+			prinf("Age: (nil)\n");
+		}
+		else if (d->age < 0)
+		{
+			prinf("Age: %f\n", d->age);
+		}
+		if (d->owner == NULL)
+		{
+			prinf("Owner: (nil)\n");
+		}
+		else if (d->owner != NULL)
+		{
+			prinf("Owner: %s\n", d->owner);
+		}
+	}
+}
