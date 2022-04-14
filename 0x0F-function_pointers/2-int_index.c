6ba35b88a2a45c645da56aec1bc654b1c6aeef87
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-04-14 14:07:27 +0100

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,7 +4,7 @@
  * array_iterator - this prints the name of a function
  * @array: the name of waht shold be printed
  * @size: this is the funcliton
- * @action: function to be passed
+ * @cmp: function to be passed
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
