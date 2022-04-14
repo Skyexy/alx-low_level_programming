13918aea3ac78d8fd720548038e0de17b75a11c6
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-04-14 15:39:03 +0100

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -6,21 +6,20 @@ int main(int argc, char *argv[])
 {
 	int num1 = atoi(argv[1]);
 	int num2 = atoi(argv[3]);
-	char c = *(argv[2]);
-	int y;
+	char *c = argv[2];
 	
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (get_op_func(c)(num1, num2) == NULL)
+	if (get_op_func(c) == NULL || c[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((c == '/' && num2 == 0) ||
-	    (c == '%' && num2 == 0))
+	if ((*c == '/' && num2 == 0) ||
+	    (*c == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
