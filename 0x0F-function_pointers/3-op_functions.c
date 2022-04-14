23dcf8bf9b7aa2c56a5b170fe28f06baef917542
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-04-14 14:40:55 +0100

    Create 3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+
+/**
+ * op_add - this prints the name of a function
+ * @a: the name of waht shold be printed
+ * @b: this is the funcliton
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - this prints the name of a function
+ * @a: the name of waht shold be printed
+ * @b: this is the funcliton
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - this prints the name of a function
+ * @a: the name of waht shold be printed
+ * @b: this is the funcliton
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - this prints the name of a function
+ * @a: the name of waht shold be printed
+ * @b: this is the funcliton
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - this prints the name of a function
+ * @a: the name of waht shold be printed
+ * @b: this is the funcliton
+ */
+int op_mod(int a, int b);
+{
+	return (a % b);
+}
