a67e70a7fd0b8882094626d2a565a0f3cf50947f
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-04-14 15:57:47 +0100

    Update 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -2,15 +2,15 @@
 #define CALC_H
 
 /**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
+ * struct op - A struct op.
+ * Description: a header file that contains all function prototypes
+ * @op: The operator.
+ * @f: The associated function.
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
