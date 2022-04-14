7dd07b67c5b9b6ec9438cf0cf965cd1d46f9a93c
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-04-14 14:13:49 +0100

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,8 +17,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 	for (i = 0; i < size; i++)
 	{
 		z = cmp(array[i]);
+		if (z > 0)
+			return (z);
 	}
-	if (z == 0)
-		return (-1);
-	return (z);
+	return (-1);
 }
