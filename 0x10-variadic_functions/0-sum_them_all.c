55d416820ff4d871ad0e4a0ac08984a0bcb340ba
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-04-15 00:22:26 +0100

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,14 +10,13 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
+	va_list vl;
 	int s = 0;
 
 	if (n == 0)
 	{
 		return (0);
 	}
-	va_list vl;
-
 	va_start(vl, n);
 	for (i = 0; i < n; i++)
 	{
