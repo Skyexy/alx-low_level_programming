d00e78ca4990de51f3f164b37d0aa5b35d6e8fcc
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-04-14 15:42:23 +0100

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -10,6 +10,7 @@ int op_mod(int a, int b);
  * op_add - this prints the name of a function
  * @a: the name of waht shold be printed
  * @b: this is the funcliton
+ * Return: (a + b)
  */
 int op_add(int a, int b)
 {
@@ -20,6 +21,7 @@ int op_add(int a, int b)
  * op_sub - this prints the name of a function
  * @a: the name of waht shold be printed
  * @b: this is the funcliton
+ * Return: (a + b)
  */
 int op_sub(int a, int b)
 {
@@ -30,6 +32,7 @@ int op_sub(int a, int b)
  * op_mul - this prints the name of a function
  * @a: the name of waht shold be printed
  * @b: this is the funcliton
+ * Return: (a + b)
  */
 int op_mul(int a, int b)
 {
@@ -40,6 +43,7 @@ int op_mul(int a, int b)
  * op_div - this prints the name of a function
  * @a: the name of waht shold be printed
  * @b: this is the funcliton
+ * Return: (a + b)
  */
 int op_div(int a, int b)
 {
@@ -50,6 +54,7 @@ int op_div(int a, int b)
  * op_mod - this prints the name of a function
  * @a: the name of waht shold be printed
  * @b: this is the funcliton
+ * Return: (a + b)
  */
 int op_mod(int a, int b)
 {
