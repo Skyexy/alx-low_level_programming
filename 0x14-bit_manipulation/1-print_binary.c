5db13d1497c5f3198747fbc2092ef185847a2e03
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-04-29 12:52:14 +0100

    Create 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+#Include <stdio.h>
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number to be changed
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	int i;
+
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	i = (n & 1);
+	putchar(i + '0');
+}
