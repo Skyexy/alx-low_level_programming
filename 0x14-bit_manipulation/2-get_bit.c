565ff8ab37f187f3278d8ac733f90392190150ce
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-04-29 14:54:16 +0100

    Create 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *  returns the value of a bit at a given index
+ * @n: the number to be changed
+ * @index: the nindex to change
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int i;
+	int o;
+
+	for (i = 0; i < index; i++)
+	{
+		n = n >> 1;
+		o = n & 1;
+	}
+	return (o);
+}
