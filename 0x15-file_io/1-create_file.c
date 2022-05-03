0162464faf51d2d53462618005fd93c8b6c5eff4
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-05-03 21:33:00 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	for(lenght = 0; text_content[lenght] != '\0'; lenght++)
+	for (lenght = 0; text_content[lenght] != '\0'; lenght++)
 	{
 	}
 	o = write(openfile, text_content, lenght);
