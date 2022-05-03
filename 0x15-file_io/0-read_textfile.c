34e0599069d0db0ee00de8b3730f1dfb7bcdfa6e
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-05-03 12:10:53 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,27 +21,24 @@ int _putchar(char c)
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-        int openfile;
-        char *buf = malloc(sizeof(char)*letters);
-        int o = 0;
- 
+	int openfile;
+	char *buf = malloc(sizeof(char)*letters);
+	int o = 0;
 
-        openfile = open(filename, O_RDONLY);
-        
-        if (openfile == -1 || filename == NULL)
-        {
-                return (0);
-        }
-        
-        read(openfile, buf, letters);
-        close(openfile);
-        while (buf[o] != '\0')
-        {
-                o += _putchar(buf[o]);
-                if (o == -1)
-                {
-                        return (0);
-                }
-        }
-        return (o);
+	openfile = open(filename, O_RDONLY);
+	if (openfile == -1 || filename == NULL)
+	{
+		return (0);
+	}
+	read(openfile, buf, letters);
+	close(openfile);
+	while (buf[o] != '\0')
+	{
+		o += _putchar(buf[o]);
+		if (o == -1)
+		{
+			return (0);
+		}
+	}
+	return (o);
 }
