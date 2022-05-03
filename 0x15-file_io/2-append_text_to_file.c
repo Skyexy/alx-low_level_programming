81b355cc690ffb8786a11713f5029a5c3c906865
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-05-03 23:19:44 +0100

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,7 +6,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	int lenght;
 	int o;
 
-	openfile = open(filename, O_APPEND);
+	openfile = open(filename, O_RDWR | O_APPEND);
 	if (openfile == -1 || filename == NULL)
 	{
 		return (-1);
