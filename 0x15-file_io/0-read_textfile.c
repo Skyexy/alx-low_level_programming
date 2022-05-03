51faeec486234687ca3c907a95f169b877c0871d
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-05-03 11:28:24 +0100

    Create 0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+        int openfile;
+        char buff[letters];
+        int o = 0;;
+
+        openfile = open(filename, O_RDONLY);
+        
+        if (openfile == -1 | filename == NULL)
+        {
+                return (0);
+        }
+        
+        read(filename, buff, letters);
+        buff[letters] = '\0';
+        close(openfile);
+        
+        o = write(STDOUT_FILENO, buff, letters);
+        if (o == -1)
+        {
+                return (0);
+        }
+        return (o);
+}
