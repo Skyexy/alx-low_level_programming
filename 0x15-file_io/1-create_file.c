25a3004e34fadf5ea4c18d25f7d3a0230af660bd
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-05-03 22:25:39 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	int lenght;
 	int o;
 
-	openfile = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+	openfile = open(filename, O_RDWR| O_CREAT| O_TRUNC, 0600);
 	if (openfile == -1 || filename == NULL)
 	{
 		return (-1);
@@ -21,11 +21,10 @@ int create_file(const char *filename, char *text_content)
 	for (lenght = 0; text_content[lenght] != '\0'; lenght++)
 	{
 	}
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
-		return (1);
+		o = write(openfile, text_content, lenght);
 	}
-	o = write(openfile, text_content, lenght);
 	if (o == -1)
 	{
 		return (-1);
