5a04263b26d2d53362fdf9635b7a5dd0c3299145
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-05-03 12:07:23 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
@@ -13,6 +12,13 @@ int _putchar(char c)
 	return (write(STDOUT_FILENO, &c, 1));
 }
 
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * @filenae: The file to be printed
+ * @letters: the number of file name to be printed;
+ * Return: On success o.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
         int openfile;
