99c24e5a84adaca05c111ba2ec3f958524aed6f2
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-05-03 22:36:01 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,10 +18,9 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (*text_content == '\0')
+	if (!text_content)
 	{
-		close(openfile);
-		return (1);
+		textfile = "";
 	}
 	for (lenght = 0; text_content[lenght] != '\0'; lenght++)
 	{
