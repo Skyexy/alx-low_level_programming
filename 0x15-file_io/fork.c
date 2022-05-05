11bbaef1b1a0a50adb2b062fdb4512a03cfa3c37
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-05-05 18:37:40 +0100

    Update fork.c

--- 0x15-file_io/fork.c
+++ 0x15-file_io/fork.c
@@ -11,17 +11,68 @@
 int main(void)
 {
     char *argv[] = {"/bin/ls", "-l", "/tmp", NULL};
-    pid_t my_pid;
+    pid_t ay_pid;
+    pid_t by_pid;
+    pid_t cy_pid;
+    pid_t dy_pid;
+    pid_t ey_pid;
     int y;
 
-    if (execve(argv[0], argv, NULL) == -1)
+    ay_pid = fork();
+    if (ay_pid == 0)
     {
-        perror("Error:");
+        if (execve(argv[0], argv, NULL) == -1)
+        {
+            perror("Error:");
+        }
     }
-    my_pid = fork();
-    if (my_pid == 0)
+    else
     {
-        printf("Wait for me, wait for me\n");
+        wait(&y);
+    }
+    by_pid = fork();
+    if (by_pid == 0)
+    {
+        if (execve(argv[0], argv, NULL) == -1)
+        {
+            perror("Error:");
+        }
+    }
+    else
+    {
+        wait(&y);
+    }
+    cy_pid = fork();
+    if (cy_pid == 0)
+    {
+        if (execve(argv[0], argv, NULL) == -1)
+        {
+            perror("Error:");
+        }
+    }
+    else
+    {
+        wait(&y);
+    }
+    dy_pid = fork();
+    if (dy_pid == 0)
+    {
+        if (execve(argv[0], argv, NULL) == -1)
+        {
+            perror("Error:");
+        }
+    }
+    else
+    {
+        wait(&y);
+    }
+    ey_pid = fork();
+    if (ey_pid == 0)
+    {
+        if (execve(argv[0], argv, NULL) == -1)
+        {
+            perror("Error:");
+        }
     }
     else
     {
