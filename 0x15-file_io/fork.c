4b0a0767ea8ed07c586a6ee8c07f777e81415a92
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-05-05 18:32:46 +0100

    Update fork.c

--- 0x15-file_io/fork.c
+++ 0x15-file_io/fork.c
@@ -10,12 +10,8 @@
  */
 int main(void)
 {
-    char *argv[] = {"ls", "-l", "/tmp", NULL};
+    char *argv[] = {"ls -l", "/tmp", NULL};
     pid_t my_pid;
-    pid_t ly_pid;
-    pid_t ky_pid;
-    pid_t ny_pid;
-    pid_t xy_pid;
     int y;
 
     if (execve(argv[0], argv, NULL) == -1)
@@ -24,42 +20,6 @@ int main(void)
     }
     my_pid = fork();
     if (my_pid == 0)
-    {
-        printf("Wait for me, wait for me\n");
-    }
-    else
-    {
-        wait(&y);
-    }
-    ly_pid = fork();
-    if (ly_pid == 0)
-    {
-        printf("Wait for me, wait for me\n");
-    }
-    else
-    {
-        wait(&y);
-    }
-    ky_pid = fork();
-    if (ky_pid == 0)
-    {
-        printf("Wait for me, wait for me\n");
-    }
-    else
-    {
-        wait(&y);
-    }
-    ny_pid = fork();
-    if (ny_pid == 0)
-    {
-        printf("Wait for me, wait for me\n");
-    }
-    else
-    {
-        wait(&y);
-    }
-    xy_pid = fork();
-    if (xy_pid == 0)
     {
         printf("Wait for me, wait for me\n");
     }
