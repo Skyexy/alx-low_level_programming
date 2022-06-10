615f55feeec6becec6823291dd21fe644789a0da
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-06-10 20:28:40 +0100

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,13 +8,13 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i = 0;
 
-	while(h -> next != NULL)
+	while (h->next != NULL)
 	{
-		printf("%d\n",h->n);
+		printf ("%d\n", h->n);
 		h = h->next;
 		i++;
 	}
-	printf("%d\n",h->n);
+	printf("%d\n", h->n);
 	i++;
-	return(i);
+	return (i);
 }
