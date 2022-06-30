5c414ea7faadeae423b5f7c819b804f8880c7e48
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-06-30 21:30:03 +0100

    Create 2-key_index.c

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * key_index - gives you the index of a key
+ * @key: string used to generate hash value
+ * @size: size of the array of the hash table
+ *
+ * Return: index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+        int l = hash_djb2(key);
+        unsigned long int index = l % size;
+        return (index);
+}
