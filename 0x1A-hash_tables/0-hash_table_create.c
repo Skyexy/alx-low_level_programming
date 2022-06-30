02e43815bc5487c83b0b0e1a0967ebcbc21dba93
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-06-30 23:51:34 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,6 +7,10 @@ hash_table_t *hash_table_create(unsigned long int size __attribute__((unused)))
 	hash_table_t *memory = malloc(sizeof(hash_table_t));
 	hash_node_t **slow_memory = malloc(sizeof(char *) * size);
 	
+	if (memory == NULL || slow_memory == NULL)
+	{
+		return (NULL);
+	}
 	memory -> size = size;
 	memory -> array = slow_memory;
 	return (memory);
