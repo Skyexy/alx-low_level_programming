05c7dd15c7dd480aa4173a4183ac48536b7fc911
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-06-30 16:19:17 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include "hash_tables.h"
 
-hash_table_t *hash_table_create(unsigned long int size)
+hash_table_t *hash_table_create(unsigned long int size __attribute__((unused)))
 {
-	hash_table_t -> size = size;
-	hash_table_t -> array = malloc(sizeof(hash_node_t) * size);
+	(hash_table_t -> size) = size;
+	(hash_table_t -> array) = malloc(sizeof(hash_node_t) * size);
 	
 	if (hash_table_t -> array == NULL)
 	{
 		return (NULL);
 	}
-	return (hash_table_t -> array);
+	return ((hash_table_t -> array));
 }
