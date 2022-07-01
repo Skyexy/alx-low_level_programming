12f2e4f1647835e894d40c5d69b72dc4cc9339bc
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-07-01 13:19:03 +0100

    Update hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,3 +1,6 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -30,3 +33,5 @@ typedef struct hash_table_s
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
+
+#endif
