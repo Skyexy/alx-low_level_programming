00658083b55268063b72cdf816c4c0792e278e82
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-07-01 15:16:35 +0100

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -23,7 +23,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	item->key = (char *)key;
 	item->value = (char *)value;
-	if (item == NULL || ht == NULL || (char *)key[0] = "")
+	if (item == NULL || ht == NULL || key[0] == "")
 	{
 		return (0);
 	}
