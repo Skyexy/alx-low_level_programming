9a5000eeeefcda127164b8fd03504736fc1aa1bf
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-10-03 21:28:08 +0100

    Create 0-linear.c

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an array of integers
+ * @array: a pointer to the first element of the array
+ * @size: number of elements in array
+ * @value: the value to search for
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int i = value;
+	size_t j
+	for (int j = 0, i < size)
+}
