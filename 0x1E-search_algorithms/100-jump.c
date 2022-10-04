3ba1f39b128981539515e034b97c36e2c1378b16
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-10-04 16:30:41 +0100

    Update 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -10,6 +10,37 @@
  *
  * Return: Always EXIT_SUCCESS
  */
+int linear_search(int *array, size_t size, int value, size_t low)
+{
+	size_t j = low;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	while (j < size)
+	{
+		printf("Value checked array[%ld] = [%d]\n", j, array[j]);
+		if (array[j] == value)
+		{
+			break;
+		}
+		j++;
+	}
+	if (j == size)
+		return (-1);
+	else
+		return (j);
+}
+
+/**
+ * jump_searc - searches for a value in an array of integers
+ * @array: a pointer to the first element of the array
+ * @size: number of elements in array
+ * @value: the value to search for
+ *
+ * Return: Always EXIT_SUCCESS
+ */
 int jump_search(int *array, size_t size, int value)
 {
 	size_t low = 0;
@@ -19,10 +50,13 @@ int jump_search(int *array, size_t size, int value)
 	{
 		return (-1);
 	}
-	while (j < size)
+	while (array[min(j, size - 1)] < value)
 	{
 		printf("Value checked array[%ld] = [%d]\n", j, array[j]);
 		low = j;
 		j += sqrt(size);
 	}
+	printf("Value found between indexes[%ld] and [%ld]\n", j, low);
+	return linear_search(array, size, value, low);
+	
 }
