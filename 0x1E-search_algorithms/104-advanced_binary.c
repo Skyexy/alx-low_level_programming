cc453846521b0043c67952b36b4aa34a265ec4e7
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-10-05 12:44:10 +0100

    Update 104-advanced_binary.c

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -28,7 +28,7 @@ void print_array(int *array, int low, int high)
  */
 int binary(int *array, int value, int low, int high)
 {
-	int mid = low + (high - low + 1) / 2;
+	int mid = (low + high) / 2;
 	int j = -1;
 	
 	print_array(array, low, high);
