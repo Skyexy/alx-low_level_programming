0660c920dd44e7b35a94963d064e88d634f7b572
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-10-05 21:25:55 +0100

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -45,5 +45,8 @@ listint_t *jump_list(listint_t *list, size_t size, int value);
 listint_t *create_list(int *array, size_t size);
 void free_list(listint_t *list);
 void print_list(const listint_t *list);
+skiplist_t *create_skiplist(int *array, size_t size);
+void free_skiplist(skiplist_t *list);
+void print_skiplist(const skiplist_t *list);
 
 #endif /* SEARCH_ALGOS_H */
